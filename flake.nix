4124014e610e1808fd496186188686c61cf578e6
Craig Hay <craig.hay@d-tacq.co.uk>
2026-03-27 11:59:05 +0000

    First commit

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Cross-compiled C program for Zynq-7000";
+  inputs = {
+    # We are pulling from unstable channel to get the latest tools
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      # The machine building on
+      buildPlatform = "x86_64-linux";
+
+      # Import standard package set for build machine
+      pkgs = import nixpkgs { system = buildPlatform; };
+      
+      # Import cross-compilation package for 32-bit ARM
+      crossPkgs = pkgs.pkgsCross.armv7l-hf-multiplatform;
+    in {
+      packages.${buildPlatform}.default = crossPkgs.stdenv.mkDerivation {
+        pname = "zynq-hello";
+        version = "0.1.0";
+
+        # Source code is current directory
+        src = ./.;
+
+        # crossPkgs.stdenv automagically sets $CC to ARM cross-compiler
+        buildPhase = ''
+          $CC -o zynq-hello main.c
+        '';
+
+        installPhase = ''
+          mkdir -p $out/bin
+          cp zynq-hello $out/bin/
+        '';
+      };
+    };
+}
---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, Zynq from Nix!\n");
+    return 0;
+}
+
